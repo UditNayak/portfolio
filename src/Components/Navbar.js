e6499fbfcefe4b35f9b0c8aa48926f4980eca1d6
UditNayak <uditnayak027@gmail.com>
2024-05-29 15:00:00 +0530

    Navbar Component

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+    return (
+      <nav>
+        <ul className="navbar">
+          <li className="nav-item">Home</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Project</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </nav>
+    );
+  }
+export default Navbar;
