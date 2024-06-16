de79328be790023f6a56679f319354e95b04cb9a
UditNayak <uditnayak027@gmail.com>
2024-06-16 22:52:36 +0530

    Updated Project Section

--- src/components/ProjectBox/ProjectBox.jsx
+++ src/components/ProjectBox/ProjectBox.jsx
@@ -5,24 +5,26 @@ import "./ProjectBox.css";
 function ProjectBox({ projectPhoto, projectName }) {
   const desc = {
     IndiaPAYDesc:
-      "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub: "https://github.com/",
-    TindogWebsite: "https://devanshsahni.github.io/tindog/",
+      "I developed a Node.js Payment Microservice that integrates RazorPay for payment processing and uses webhooks to capture payment statuses. It also stores lead information in Zoho CRM for efficient customer management. This solution streamlines both payment workflows and customer relationship tracking.",
+    IndiaPAYGithub:
+      "https://github.com/UditNayak/Backend-Payment-Micro-Services",
+    IndiaPAYWebsite:
+      "https://github.com/UditNayak/Backend-Payment-Micro-Services",
 
     ImageEditorDesc:
-      "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub: "https://github.com/",
-    TindogWebsite: "https://devanshsahni.github.io/tindog/",
+      "I created a Java CLI Image Editor using the BufferedImage library, offering features like grayscale conversion, rotation, brightness adjustment, inversion, blurring, edge detection, and special effects.",
+    ImageEditorGithub: "https://github.com/UditNayak/Image-Editor-Java",
+    ImageEditorWebsite: "https://github.com/UditNayak/Image-Editor-Java",
 
     AerofitDesc:
-      "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub: "https://github.com/",
-    TindogWebsite: "https://devanshsahni.github.io/tindog/",
+      "I developed a business case study on Aerofit Data using Exploratory Data Analysis (EDA) to help categorize the right customers for their projects. This data science project enhances customer segmentation and targeting strategies.",
+    AerofitGithub: "https://github.com/UditNayak/Aerofit-Business-Case-Study",
+    AerofitWebsite: "https://github.com/UditNayak/Aerofit-Business-Case-Study",
 
     Scrape_N_ShopDesc:
-      "This website is a landing page of Tinder but for dogs. It is a responsive website which was made to understand Bootstrap. I also learned how to host my project on Github and then how to deploy that project using Github pages.",
-    TindogGithub: "https://github.com/",
-    TindogWebsite: "https://devanshsahni.github.io/tindog/",
+      "I created a Python web scraper using BeautifulSoup that extracts product details from various websites. It helps users make informed purchasing decisions by comparing prices and features across different platforms.",
+    Scrape_N_ShopGithub: "https://github.com/UditNayak/Price-Comparison",
+    Scrape_N_ShopWebsite: "https://github.com/UditNayak/Price-Comparison",
   };
 
   let show = "";
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,4 +1,7 @@
-import dog from "../../images/dog.png";
+import img1 from "../../images/img1.png";
+import img2 from "../../images/img2.jpeg";
+import img3 from "../../images/img3.webp";
+import img4 from "../../images/img4.png";
 import ProjectBox from "../ProjectBox/ProjectBox";
 import Footer from "../Footer";
 import "./Projects.css";
@@ -10,10 +13,10 @@ function Projects() {
         My <b>Projects</b>
       </h1>
       <div className="project">
-        <ProjectBox projectPhoto={dog} projectName="IndiaPAY" />
-        <ProjectBox projectPhoto={dog} projectName="ImageEditor" />
-        <ProjectBox projectPhoto={dog} projectName="Aerofit" />
-        <ProjectBox projectPhoto={dog} projectName="Scrape_N_Shop" />
+        <ProjectBox projectPhoto={img1} projectName="ImageEditor" />
+        <ProjectBox projectPhoto={img2} projectName="Aerofit" />
+        <ProjectBox projectPhoto={img3} projectName="IndiaPAY" />
+        <ProjectBox projectPhoto={img4} projectName="Scrape_N_Shop" />
       </div>
       <Footer />
     </div>
